def318a6043381c1757448d8bf3a00c8c113b0ae
ANGELA <angela.mbithe@student.moringaschool.com>
2021-06-13 22:34:32 +0300

    add authenticate forms

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,6 @@ from flask_login import login_user,login_required,logout_user
 from .forms import RegForm,LoginForm
 from ..models import User
 from .. import db
-from ..email import mail_message
 
 @auth.route('/login', methods = ['GET','POST'])
 def login():
@@ -15,7 +14,7 @@ def login():
             login_user(user,form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or Password')
-    return render_template('auth/login.html', loginform = form)
+    return render_template('auth/login.html', form = form)
 
 @auth.route('/logout')
 @login_required
@@ -29,6 +28,5 @@ def signup():
     if form.validate_on_submit():
         user = User(email = form.email.data, username = form.username.data, password = form.password.data)
         user.save_u()
-        mail_message("Welcome to Pitch-World","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
-    return render_template('auth/signup.html', r_form = form)
+    return render_template('auth/register.html', registration_form = form)
